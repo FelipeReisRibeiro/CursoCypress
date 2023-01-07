de2e67cfc87f73ec79fd2f2140db26cb9fe7db85
Felipe Reis <felipe.ribeiro@carteriaglobal.com>
2023-01-06 21:35:28 -0300

    Adicionando ao ci pipeline

--- cypress/integration/TestandoPoliticaPrivacy.spec.js
+++
@@ -1,5 +0,0 @@
-it('Testa a pagina de politica de privacidade de forma independente', function() {  //Muito importante este teste
-    cy.visit('./src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
-    
-})
